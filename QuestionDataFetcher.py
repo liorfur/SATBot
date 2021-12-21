e31dfa7ecd62e943a6d70420d5f2d1519c62d374
liorfur <lior.furman10@gmail.com>
2021-12-21 15:29:20 +0200

    Initial commit

---
+++ QuestionDataFetcher.py
@@ -0,0 +1,18 @@
+class getQuestionData:
+
+    def __init__(self, url):
+        self.url = url
+        self.data = None
+        
+    def getHtmlContent(self):
+        htmlContent = requests.get(self.url).text
+        self.data = BeautifulSoup(htmlContent, "lxml")
+            
+    def getAnswers(self):
+        self.getHtmlContent()
+        answerstbodys = self.data.find("div", attrs={"class": "datagrid"}).table.findAll("tbody")
+        answerList = []
+        for tbody in answerstbodys:
+            for td in tbody.tr.findAll("td"):
+                answerList.append(td.text)
+        return answerList
---
+++ README.md
@@ -0,0 +1,2 @@
+# Project
+     Statistic tool for the psychometry (SAT) test in Israel collects     Data from Yoel Geva's website and returns the most frequent answer for     each question     Author: Lior Furman
